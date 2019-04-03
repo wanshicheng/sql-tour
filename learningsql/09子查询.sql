344f211a86e888d5d317b2e8d7bf87afb63d7359
万仕诚 <wanshicheng@qq.com>
2019-04-03 08:55:01 +0800

    Update 09子查询.sql

--- learningsql/09子查询.sql
+++ learningsql/09子查询.sql
@@ -26,8 +26,51 @@ WHERE open_emp_id <> (SELECT e.emp_id
 FROM employee e INNER JOIN branch b
 ON e.`assigned_branch_id` = b.`branch_id`
 WHERE e.`title` = 'Head Teller' AND b.`city`= 'Woburn');
+	# 如果在等式条件下使用子查询
+	# 而子查询又返回多行结果，将会出错
+SELECT account_id, product_cd, cust_id, avail_balance
+FROM account
+WHERE open_emp_id <> (SELECT e.emp_id
+FROM employee e INNER JOIN branch b
+ON e.`assigned_branch_id` = b.`branch_id`
+WHERE e.`title` = 'Teller' AND b.`city`= 'Woburn');
 
+# 单独运行子查询，会返回多行
+
+SELECT e.emp_id
+FROM employee e INNER JOIN branch b
+ON e.`assigned_branch_id` = b.`branch_id`
+WHERE e.`title` = 'Teller' AND b.`city`= 'Woburn';
+
+# 多行单列子查询
+	# in 和 not in 运算符
+	# 检查一个值集中能否包含某一个值
+SELECT branch_id, NAME, city
+FROM branch
+WHERE NAME IN ('Headquarters', 'Quincy Branch');
+
+SELECT branch_id, NAME, city
+FROM branch
+WHERE NAME = 'Headquarters' OR NAME =  'Quincy Branch';
+
+
+SELECT emp_id, fname, lname, title
+FROM employee
+WHERE emp_id IN (SELECT superior_emp_id
+FROM employee);
+
+SELECT superior_emp_id
+FROM employee;
+
+SELECT emp_id, fname, lname, title
+FROM employee
+WHERE emp_id NOT IN (SELECT superior_emp_id
+FROM employee
+WHERE superior_emp_id IS NOT NULL);
 
 #关联子查询
 	#在跟新或删除语句中经常会用到
+	
+	
+	
 
